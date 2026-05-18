42fbbcf955666f5859547637b75e2625f4bf0603
WiltonReis <wiltonfilho0825@gmail.com>
2026-05-18 14:51:18 -0300

    feat: adiciona prometheus

--- src/main/java/com/ordersystem/config/SecurityConfig.java
+++ src/main/java/com/ordersystem/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/health", "/actuator/prometheus").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
